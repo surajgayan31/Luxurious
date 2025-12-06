a1fcae8bd9f9fe2dc69c1a8113dd20731d6b9a79
Suraj Gayan <surajgayan31@gmail.com>
2025-12-07 00:07:53 +0530

    some issue

--- src/screens/auth/login/Login.tsx
+++ src/screens/auth/login/Login.tsx
@@ -1,26 +1,16 @@
 import React, { useRef, useState } from 'react';
-import {
-  View,
-  Text,
-  Image,
-  StatusBar,
-} from 'react-native';
+import { View, Text, Image, StatusBar } from 'react-native';
 import CustomButton from '../../../component/customButton/CustomButton';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './style';
-import { color,  fontSize } from '../../../styles/styles';
+import { color, fontSize } from '../../../styles/styles';
 import RightIconInput from '../../../component/customTextInput/RightIconInput';
 import { Utils } from '../../../utils/Utils';
 import RightIconDisplayInput from '../../../component/customTextInput/RightIconDisplayInput';
 import navigationService from '../../../routes/navigationService';
 import { routes } from '../../../routes/stacks';
 import { validateMethod } from '../../../component/validationMethod/validationMethods';
-
-
-
 const Login = () => {
-
-   
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
@@ -49,7 +39,9 @@ const Login = () => {
       return;
     }
     if (!passwordPattern.test(val)) {
-      setPasswordError('Password must be at least 8 chars and alphanumeric (letters + numbers)');
+      setPasswordError(
+        'Password must be at least 8 chars and alphanumeric (letters + numbers)',
+      );
     } else {
       setPasswordError('');
     }
@@ -57,107 +49,105 @@ const Login = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
-       
-          <View style={styles.column}>
-            <Image
-              source={require('../../../assets/images/logo/splash_logo2.png')}
-              style={styles.applogo}
-            />
-          </View>
-          <View style={styles.view2}>
-            <Text style={styles.logoText1}>{'WELCOME BACK! :)'}</Text>
-            <Text style={styles.text21}>{'Access your account:'}</Text>
-          </View>
-          <View style={{ marginBottom: Utils.calculateHeight(2) }}>
-            <RightIconDisplayInput
-              value={email}
-              onChangeText={onChangeEmail}
-              placeholder="@Email"
-              rightIcon={require('../../../assets/images/icon/mail.png')}
-              keyboardType="default"
-            />
-            {emailError.length > 0 && (
-              <Text style={{ color: 'red', fontSize: 12, marginTop: 4 }}>{emailError}</Text>
-            )}
 
-            <RightIconInput
-              value={password}
-              onChangeText={onChangePassword}
-              placeholder="Password"
-              rightIcon={eyeIcon}
-              onRightIconPress={togglePasswordVisibility}
-              secureTextEntry={secureTextEntry}
-              // intentionally not passing validationType to use our simpler alphanumeric rule
-            />
-            {passwordError.length > 0 && (
-              <Text style={{ color: 'red', fontSize: 12, marginTop: 4 }}>{passwordError}</Text>
-            )}
-          </View>
-          <CustomButton
-            text="Next"
-            marginVertical={Utils.calculateHeight(0)}
-            width={'100%'}
-            
-            borderRadius={10}
-            onPress={async () => {
-              // TODO: replace with real authentication call
-              console.log('Login submitted', { email, password });
-              const userData = {
-                email,
-                // In a real app, save the token and user info returned by server
-                token: 'demo-token',
-                userId: email,
-              };
-              await Utils._setUserData(userData);
-              navigationService.navigate(routes.Product);
-            }}
-            height={50}
-            disable={
-              !email || !password || emailError.length > 0 || passwordError.length > 0
-            }
-          />
-          <View  style={styles.forgotPasswordContainer}>
-            <Text style={styles.forgotPasswordText}>
-              Forgot{' '}
-              <Text
-                style={{
-                  fontWeight: '700',
-                  fontSize: fontSize.size_18,
-                  color: color.primary,
-                }}
-              >
-                Password?
-              </Text>
-            </Text>
-          </View>
-          <View
+      <View style={styles.column}>
+        <Image
+          source={require('../../../assets/images/logo/splash_logo2.png')}
+          style={styles.applogo}
+
+          
+        />
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.logoText1}>{'WELCOME BACK! :)'}</Text>
+        <Text style={styles.text21}>{'Access your account:'}</Text>
+      </View>
+      <View style={{ marginBottom: Utils.calculateHeight(2) }}>
+        <RightIconDisplayInput
+          value={email}
+          onChangeText={onChangeEmail}
+          placeholder="@Email"
+          rightIcon={require('../../../assets/images/icon/mail.png')}
+          keyboardType="default"
+        />
+        {emailError.length > 0 && (
+          <Text style={{ color: 'red', fontSize: 12, marginTop: 4 }}>
+            {emailError}
+          </Text>
+        )}
+
+        <RightIconInput
+          value={password}
+          onChangeText={onChangePassword}
+          placeholder="Password"
+          rightIcon={eyeIcon}
+          onRightIconPress={togglePasswordVisibility}
+          secureTextEntry={secureTextEntry}
+          // intentionally not passing validationType to use our simpler alphanumeric rule
+        />
+        {passwordError.length > 0 && (
+          <Text style={{ color: 'red', fontSize: 12, marginTop: 4 }}>
+            {passwordError}
+          </Text>
+        )}
+      </View>
+      <CustomButton
+        text="Next"
+        marginVertical={Utils.calculateHeight(0)}
+        width={'100%'}
+        borderRadius={10}
+        onPress={async () => {
+          // TODO: replace with real authentication call
+          console.log('Login submitted', { email, password });
+          const userData = {
+            email,
+            // In a real app, save the token and user info returned by server
+            token: 'demo-token',
+            userId: email,
+          };
+          await Utils._setUserData(userData);
+          navigationService.navigate(routes.Product);
+        }}
+        height={50}
+        disable={
+          !email ||
+          !password ||
+          emailError.length > 0 ||
+          passwordError.length > 0
+        }
+      />
+      <View style={styles.forgotPasswordContainer}>
+        <Text style={styles.forgotPasswordText}>
+          Forgot{' '}
+          <Text
             style={{
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              marginTop: Utils.calculateHeight(14),
+              fontWeight: '700',
+              fontSize: fontSize.size_18,
+              color: color.primary,
             }}
           >
-            <View >
-              
-            <Text style={{ fontSize: fontSize.size_18 }}>
-              {'Don’t have an account? '}
-              <Text style={{ fontWeight: 'bold', color: color.primary }}>
-                {'Sign up!'}
-              </Text>
+            Password?
+          </Text>
+        </Text>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          marginTop: Utils.calculateHeight(14),
+        }}
+      >
+        <View>
+          <Text style={{ fontSize: fontSize.size_18 }}>
+            {'Don’t have an account? '}
+            <Text style={{ fontWeight: 'bold', color: color.primary }}>
+              {'Sign up!'}
             </Text>
-            </View>
-          </View>
-      
-      
-      
-       
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default Login;
-
-
-
-
-
